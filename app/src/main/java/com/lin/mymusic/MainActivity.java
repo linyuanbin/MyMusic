079465577c4e121cf2d7e7b9c623f91c6cd445af
linyuanbin <893239524@qq.com>
2016-11-01 21:26:50 +0800

    完善目录

--- app/src/main/java/com/lin/mymusic/MainActivity.java
+++ app/src/main/java/com/lin/mymusic/MainActivity.java
@@ -1,36 +1,33 @@
 package com.lin.mymusic;
 
+import android.app.Activity;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Environment;
-//import android.support.v4.widget.SearchViewCompatIcs;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.AdapterViewFlipper;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.PrivateKey;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 //Tools--> android-->Device monitor  音乐文件放入sd卡
 /*
@@ -47,10 +44,12 @@ import java.util.regex.Pattern;
  // 设置音乐播放完的监听
     mediaPlayer.setOnCompletionListener(this);
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements Runnable{
 
     private static int p=0;
    // private EditText mEditText;
+    private Spinner mSpinner;
+    private ArrayAdapter mAdapter;
     private TextView mName;
     private SeekBar mSeekBar;
     private String path;
@@ -58,10 +57,12 @@ public class MainActivity extends AppCompatActivity {
     private boolean pause; //标记是否暂停
     private int position;//用于记录播放进度
     private List<String> list;
+    private List<String> name;
     private ImageButton pauseButton;
     private AdapterViewFlipper mViewFlipper;
     int []iconId={R.mipmap.img_1,R.mipmap.img_2,R.mipmap.img_3,R.mipmap.img_4,R.mipmap.img_5,
             R.mipmap.img_6,R.mipmap.img_7,R.mipmap.img_8,R.mipmap.img_9};
+    private TextView textView_name;
 
 
  /*   private  Handler handler;
@@ -69,15 +70,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+
         list=new ArrayList<>();//用于存放音乐文件路径
+        name=new ArrayList<String>();//用于存放歌曲名字
         mMediaPlayer=new MediaPlayer();
     //    mEditText= (EditText) findViewById(R.id.filename);
         mName= (TextView) findViewById(R.id.tv_name);
         mSeekBar= (SeekBar) findViewById(R.id.seekbar);
         pauseButton= (ImageButton) findViewById(R.id.pause_button);
         mViewFlipper= (AdapterViewFlipper) findViewById(R.id.adapterviewflipper);
+        textView_name= (TextView) findViewById(R.id.text_name);
+
+
+
+
+        //mSpinner.setOnItemSelectedListener( new SpinnerListener());//点击监听
 
         BaseAdapter adapter=new BaseAdapter() { //AdapterViewFlipper的适配器
             @Override
@@ -117,32 +127,66 @@ public class MainActivity extends AppCompatActivity {
 
         search();
         init();
-        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() { //监听一首歌结束
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) { //自动循环播放
-                if (p == (list.size() - 1)) p = 0;
-                path = list.get(++p);
-                if (path != null) {
-                    if (handler.post(updateThread)) {
+        mMediaPlayer.setOnCompletionListener(new mMediaPlayersetOnCompletionListener());
 
-                    } else {
-                        handler.post(updateThread);  //如果没开启多线程则开启  以更新进度条
-                    }
-                    play();
-                    mName.setText(list.get(p));
-                    mViewFlipper.startFlipping();//开始自动播放图片
-                }else {
-                    mViewFlipper.stopFlipping();//停止播放图片
-                    Toast.makeText(MainActivity.this,"没有歌曲了哦！",Toast.LENGTH_SHORT).show();
-                }
+        //由于以下带码用到search()函数得到的数据所有要在调用search()之后执行，否则出错 //可能是由于线程的缘故
+        mSpinner=(Spinner)findViewById(R.id.spinner);
+        //1.为下拉列表定义一个数组适配器，这个数组适配器就用到里前面定义的list。装的都是list所添加的内容
+        mAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,name);//样式为原安卓里面有的android.R.layout.simple_spinner_item，让这个数组适配器装list内容。
+        //2.为适配器设置下拉菜单样式。adapter.setDropDownViewResource
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //3.以上声明完毕后，建立适配器,有关于sipnner这个控件的建立。用到myspinner
+        mSpinner.setAdapter(mAdapter);
+        mSpinner.setSelection(0, false);
+        //4.为下拉列表设置各种点击事件，以响应菜单中的文本item被选中了，用setOnItemSelectedListener
+        mSpinner.setOnItemSelectedListener(new SpinnerListener());
+                /*Spinner.OnItemSelectedListener() {   //选择item的选择点击监听事件
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                Toast.makeText(MainActivity.this,""+arg2,Toast.LENGTH_SHORT).show();
             }
-        });
 
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });*/
 
     } //onCreate
 
+    @Override
+    public void run() {
 
+    }
 
+    /*class SpinnerSelectedListener implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            textView_name.setText(""+mArrayAdapter.getItem(i));
+        }
+    }*/
+
+    class mMediaPlayersetOnCompletionListener implements MediaPlayer.OnCompletionListener{ //一首歌播放结束监听
+
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            //自动循环播放
+            if (p == (list.size() - 1)) p = 0;
+            path = list.get(++p);
+            if (path != null) {
+                if (handler.post(updateThread)) {
+
+                } else {
+                    handler.post(updateThread);  //如果没开启多线程则开启  以更新进度条
+                }
+                play();
+                mName.setText(name.get(p));
+                mViewFlipper.startFlipping();//开始自动播放图片
+            }else {
+                mViewFlipper.stopFlipping();//停止播放图片
+                Toast.makeText(MainActivity.this,"没有歌曲了哦！",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     Handler handler = new Handler(); //多线程
     Runnable updateThread = new Runnable(){
@@ -158,9 +202,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void init(){
+
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {  //seekBar进度监听
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {   
                 // fromUser判断是用户改变的滑块的值
                 if (fromUser == true) {
                     position=progress;
@@ -177,7 +222,7 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) { //拖动结束调用
                 seekBar.setProgress(seekBar.getProgress());
 
-                int test=seekBar.getProgress();
+                //int test=seekBar.getProgress();
 
                 int max=mMediaPlayer.getDuration();// 歌曲进度的最大值
 
@@ -195,6 +240,8 @@ public class MainActivity extends AppCompatActivity {
                 String s = file[i].getAbsolutePath();
                 if(s.toLowerCase().endsWith(".mp3")){  //检索得到MP3文件
                     list.add(s);
+                    String str=getFileName(s);
+                    name.add(str); //保持歌曲名
                 }
             }
 
@@ -264,28 +311,32 @@ public class MainActivity extends AppCompatActivity {
              //   File audio=new File(Environment.getExternalStorageDirectory(),filename);
               //  if(audio.exists()){ //发现文件
                 //    path=audio.getAbsolutePath();//获得绝对路径
-                path=list.get(p);
+                if(list!=null) {
+                    path = list.get(p);
                     play();//播放
                     handler.post(updateThread);//启动线程
                     mViewFlipper.startFlipping();//图片开始变换
-                //}else{
+                    //}else{
                     //path=null;
-                //    Toast.makeText(MainActivity.this,"sorry the file inexistence !",Toast.LENGTH_SHORT).show();
-                //}
+                    //    Toast.makeText(MainActivity.this,"sorry the file inexistence !",Toast.LENGTH_SHORT).show();
+                    //}
+                }else {
+                    Toast.makeText(MainActivity.this,"当前文件夹还没有歌曲哦！",Toast.LENGTH_SHORT).show();
+                }
             break;
             case R.id.pause_button: //暂停
                     if(mMediaPlayer.isPlaying()){
                         mMediaPlayer.pause();//暂停
                         pause=true;//暂停标记
                      //   ((Button)view).setText("继续"); //将View强转为button类型
-                        pauseButton.setImageResource(R.drawable.star);
+                        pauseButton.setImageResource(R.drawable.stop);
                         mViewFlipper.stopFlipping();//图片停止播放
                     }else {
                         if(pause){ //如果被暂停过则继续播放
                             mMediaPlayer.start();
                             pause=false;
                         //    ((Button)view).setText("暂停");
-                            pauseButton.setImageResource(R.drawable.stop);
+                            pauseButton.setImageResource(R.drawable.star);
                             mViewFlipper.startFlipping();//开始播放图片
                         }
                     }
@@ -311,6 +362,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void play() {  //自定义播放函数
         mMediaPlayer.reset();//把各项参数恢复到初始状态
+        pauseButton.setImageResource(R.drawable.star);
         try {
             if(path!=null) {
                 mMediaPlayer.setDataSource(path);//设置路径
@@ -339,26 +391,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public String getFileName(String pathandname){ //通过绝对路径的到文件名
+
+        int start=pathandname.lastIndexOf("/");
+        int end=pathandname.lastIndexOf(".");
+        if(start!=-1 && end!=-1){
+            return pathandname.substring(start+1,end);
+        }else{
+            return null;
+        }
+    }
 
 
     public void next(View v) {
-        if (p == (list.size() - 1)) p = 0;
-        path = list.get(++p);
-        if (path != null) {
-            if (handler.post(updateThread)) {
+        if(list!=null) {
+            if (p == (list.size() - 1)) p = 0;
+            path = list.get(++p);
+            if (path != null) {
+                if (handler.post(updateThread)) {
 
+                } else {
+                    handler.post(updateThread);  //如果没开启多线程则开启  以更新进度条
+                }
+                play();
+                //mName.setText(list.get(p));
+                //String str=getFileName(path);
+                mName.setText(name.get(p));
+                mViewFlipper.startFlipping();//开始自动播放图片
             } else {
-                handler.post(updateThread);  //如果没开启多线程则开启  以更新进度条
+                Toast.makeText(MainActivity.this, "没有歌曲了哦！", Toast.LENGTH_SHORT).show();
             }
-            play();
-            mName.setText(list.get(p));
-            mViewFlipper.startFlipping();//开始自动播放图片
         }else {
-            Toast.makeText(MainActivity.this,"没有歌曲了哦！",Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this,"文件夹为空！",Toast.LENGTH_SHORT).show();
         }
     }
 
     public void up(View v) {
+        if(list!=null){
         if (p == 0) p = list.size();
         path = list.get(--p);
         if (path != null) {
@@ -368,12 +437,41 @@ public class MainActivity extends AppCompatActivity {
                 handler.post(updateThread);  //如果没开启多线程则开启  以更新进度条
             }
             play();
-            mName.setText(list.get(p));
+            //mName.setText(list.get(p));
+            //String string=getFileName(path);
+            mName.setText(name.get(p));
             mViewFlipper.startFlipping();//开始播放图片
         }else{
             Toast.makeText(MainActivity.this,"没有歌曲了哦！",Toast.LENGTH_SHORT).show();
         }
+        }else{
+            Toast.makeText(MainActivity.this,"当前文件夹为空！",Toast.LENGTH_SHORT).show();
+        }
     }
 
+    private class SpinnerListener implements AdapterView.OnItemSelectedListener { //spinner点击监听
 
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+           // Log.i("---------->position=",""+i);
+           // textView_name.setText(i);
+            //Toast.makeText(MainActivity.this,""+i,Toast.LENGTH_SHORT).show();
+            p=i;
+            path = list.get(p);
+                    if (handler.post(updateThread)) {
+
+                    } else {
+                        handler.post(updateThread);  //如果没开启多线程则开启  以更新进度条
+                    }
+                    play();
+                    mName.setText(name.get(p));
+                    mViewFlipper.startFlipping();//开始自动播放图片
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    }
 }
